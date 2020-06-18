76edbcbdabb8eaed0465a8d128b2cabdf4b41f36
Emma Dunn <evd1@rice.edu>
2020-06-18 14:19:35 +0000

    Pass initial testsgit status!

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -24,11 +24,9 @@ import java.util.Set;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    List<TimeRange> times = new ArrayList<>();
-    List<TimeRange> newTimes = new ArrayList<>();
+    Set<TimeRange> newTimes = new HashSet<>();
     if (request.getDuration() <= TimeRange.END_OF_DAY - TimeRange.START_OF_DAY) {
         // before checking the event availability, the entire day is available
-        times.add(TimeRange.WHOLE_DAY);
         newTimes.add(TimeRange.WHOLE_DAY);
         // iterate through the people in the message request
         for (String person : request.getAttendees()) {
@@ -37,6 +35,8 @@ public final class FindMeetingQuery {
                 Set<String> curAttending = mtg.getAttendees();
                 TimeRange curWhen = mtg.getWhen();
                 if (curAttending.contains(person)) {
+                    List<TimeRange> times = new ArrayList<>();
+                    times.addAll(newTimes);
                     // iterate through the available times
                     for (TimeRange time : times) {
                         if (time.contains(curWhen)) {
@@ -60,22 +60,9 @@ public final class FindMeetingQuery {
             }
         }  
     }
-    System.out.println("1" + newTimes);
-    Boolean check = true;
-    while(check) {
-        List<TimeRange> newList = checkOverlap(newTimes);
-        if (newList == newTimes) {
-            check = false;
-        } else {
-            newTimes = newList;
-        }
-    }
-    System.out.println("2" + newTimes);
-    //Set<TimeRange> setTimes = new HashSet<>();
-    //setTimes.addAll(newTimes);
     List<TimeRange> finalTimes = new ArrayList<>();
     for (TimeRange time : newTimes) {
-        if (time.duration() != 0) {
+        if (time.duration() != 0 && request.getDuration() <= time.duration()) {
             if (finalTimes.size() == 0) {
                 finalTimes.add(time);
             } else {
@@ -96,36 +83,7 @@ public final class FindMeetingQuery {
             }
         }
     }
-    System.out.println("3" + finalTimes);
     return finalTimes;
   }
-
-  public List<TimeRange> checkOverlap(List<TimeRange> list) {
-    Set<TimeRange> newTimes = new HashSet<>();
-    for (TimeRange items1: list) {
-        for (TimeRange items2: list) {
-            // check to skip itself in the second iteration (can only be one of each element b/c passed through a set)
-            if (items1 != items2) {
-                if (items1.contains(items2)) {
-                    newTimes.add(items2);
-                } else if (items1.overlaps(items2)) {
-                    if (items1.contains(items2.start())) {
-                        newTimes.add(TimeRange.fromStartEnd(items2.start(), items1.end(), false));
-                    } else if (items2.contains(items1.start())) {
-                        newTimes.add(TimeRange.fromStartEnd(items1.start(), items2.end(), false));
-                    }
-                } else {
-                    newTimes.add(items1);
-                }
-            }
-        }
-    }
-    List<TimeRange> finalList = new ArrayList<>();
-    finalList.addAll(newTimes);
-    return finalList;
-  }
-
-  public Boolean checkAgain(List<TimeRange> list) {
-      
-  }
 }
+
